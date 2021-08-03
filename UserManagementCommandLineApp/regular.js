ed866a5fa255a981428da13afefb60bb6d1abce8
MaxManthey <m.manthey@axial3d.com>
2021-08-03 17:57:27 +0100

    added basic functionality for app

--- UserManagementCommandLineApp/regular.js
+++ UserManagementCommandLineApp/regular.js
@@ -1,5 +1,80 @@
+const prompt = require('prompt-sync')();
+
+let user = undefined
+
+module.exports = function (userObj) {
+    user = userObj
+    printNewLines(3)
+    console.log('Welcome ' + user.username)
+    printNewLines(1)
+    mainFunctionality()
+}
 
 
-module.exports = function () {
-    console.log("In regular activity")
+const mainFunctionality = () => {
+    const possibleOptions = [1,2,3,4]
+    
+    console.log('What do you want to do?')
+    console.log('(1) Rent a book')
+    console.log('(2) Return a book')
+    console.log('(3) Account settings')
+    console.log('(4) Exit')
+    
+    const choosenOption = parseInt(prompt('Please enter a number: '));
+    if(possibleOptions.includes(choosenOption)) {
+        switch(choosenOption) {
+            case 1:
+                rentABook()
+                break;
+            case 2:
+                returnABook()
+                break;
+            case 3:
+                accountSettings()
+                break;
+            case 4:
+                exitApp()
+                break;
+        }
+    } else {
+        printNewLines(1)
+        console.log("Input was not an option")
+        printNewLines(3)
+        mainFunctionality()
+    }
+}
+
+
+const rentABook = () => {
+    console.log('Rent book')
+    mainFunctionality()
+}
+
+
+const returnABook = () => {
+    console.log('Return a book')
+    mainFunctionality()
+}
+
+
+const accountSettings = () => {
+    console.log('Account settings')
+    mainFunctionality()
+}
+
+
+const exitApp = () => {
+    printNewLines(2)
+    console.log('Goodbye!')
+    printNewLines(2)
+    process.exit(1)
+}
+
+
+const printNewLines = (amount) => {
+    let toPrint = ''
+    for(let i = 0; i < amount; ++i) {
+        toPrint += '\n'
+    }
+    console.log(toPrint)
 }
--- UserManagementCommandLineApp/script.js
+++ UserManagementCommandLineApp/script.js
@@ -1,21 +1,33 @@
 const prompt = require('prompt-sync')();
 
 const login = require('./login')
-const main = require('./admin')
 const regular = require('./regular');
 const admin = require('./admin');
 
+const asciiBook = `
+      __...--~~~~~-._   _.-~~~~~--...__
+    //               'V'               \\\\ 
+   //                 |                 \\\\ 
+  //__...--~~~~~~-._  |  _.-~~~~~~--...__\\\\ 
+ //__.....----~~~~._\\ | /_.~~~~----.....__\\\\
+====================\\\\|//====================
+                    '---'
+`
+console.log(asciiBook)
+console.log("\t    WELCOME TO xyz LIBRARY\n\n")
 
 const user = login()
 
 if(Object.keys(user).length != 0) {
     if(user.admin == true) {
         const asAdmin = prompt('Would you like to log in as ADMIN?(y/n): ');
-        if(asAdmin == "y") admin()
-        else regular()
+        if(asAdmin == "y") admin(user)
+        else regular(user)
     } else {
-        regular()
+        regular(user)
     }
+} else {
+    console.log("\n\nGoodbye!\n\n")
 }
 
 
