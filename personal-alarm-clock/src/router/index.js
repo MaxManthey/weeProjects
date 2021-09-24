14eeef2bce721644277e6a3c5b891705b2716c5d
Max Manthey <mantheymax23@gmail.com>
2021-09-24 02:20:05 +0200

    setup alarm clock and login

---
+++ personal-alarm-clock/src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/alarm-clock",
+    name: "AlarmClock",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AlarmClock.vue"),
+  },
+  {
+    path: "/create-user",
+    name: "CreateUser",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/CreateUser.vue"),
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
---
+++ personal-alarm-clock/src/views/AlarmClock.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>AlarmClock</div>
+  <!-- - display current time
+       - set alarm clock
+       - checkbox to make alarm clock daily  -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
